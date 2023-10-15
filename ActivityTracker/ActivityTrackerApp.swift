785e807d1cc1fd6ab0cfedec06909b5bcff986c9
Omarep <omar.petricevic@gmail.com>
2023-10-15 16:10:47 +0200

    i i wq:
    wq:
    q:
    :wq
    :q
    :quit

---
+++ ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by Omar Petričević on 14.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityTracker/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  ActivityTracker
+//
+//  Created by Omar Petričević on 14.10.2023..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Text("ProbavamGitatovo")
+            Text("Svašta nešta")
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
