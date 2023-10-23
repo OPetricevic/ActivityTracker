70de0ec00270564891e71d83b6bd3ddbe6c0facd
Omarep <omar.petricevic@gmail.com>
2023-10-23 21:41:39 +0200

    Commiting changes, made Settings & Add menu

---
+++ ActivityTracker/Activity/View/AddWorkoutView.swift
@@ -0,0 +1,24 @@
+//
+//  AddActivityView.swift
+//  ActivityTracker
+//
+//  Created by Omar Petričević on 23.10.2023..
+//
+
+import SwiftUI
+
+struct AddWorkoutView: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 12){
+                
+            }.navigationTitle("Add Workout")
+        }
+    }
+}
+
+struct AddActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddWorkoutView()
+    }
+}
